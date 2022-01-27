bf3c3ac6b1c77bf2ac4dfa3e856a6495500f3e1d
Abbasov <tu201806001@aztu.local>
2022-01-27 14:10:34 +0400

    Den

---
+++ DenTemplate/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DenTemplate.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DenTemplate.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+             
+        }
+        public DbSet<About> Abouts { get; set; }
+    }
+}
---
+++ DenTemplate/Models/About.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DenTemplate.Models
+{
+    public class About
+    {
+        [Key]
+        public int Id{ get; set; }
+        [NotMapped]
+        public IFormFile Image{ get; set; }
+        [MaxLength (150)]
+        public string ImageFile { get; set; }
+        [MaxLength(50)]
+        public string ProductName{ get; set; }
+        [MaxLength(300)]
+        public string Context { get; set; }
+    }
+}
